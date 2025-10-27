fde89591444414ff9923f26b03de78eb07c23bb4
dvktdr78 <kei1221@ajou.ac.kr>
2025-10-27 23:44:56 +0900

    feat(main): main 함수 완성

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,21 @@
 package racingcar;
 
-public class Application {
+import racingcar.controller.RacingGameController;
+import racingcar.domain.strategy.MoveStrategy;
+import racingcar.domain.strategy.RandomMoveStrategy;
+import racingcar.validator.InputValidator;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+import racingcar.view.RacingConsole;
+
+public final class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputValidator validator = new InputValidator();
+        InputView inputView = new InputView(validator);
+        OutputView outputView = new OutputView();
+        RacingConsole console = new RacingConsole(inputView, outputView);
+        MoveStrategy moveStrategy = new RandomMoveStrategy();
+        RacingGameController controller = new RacingGameController(console, moveStrategy);
+        controller.run();
     }
 }
